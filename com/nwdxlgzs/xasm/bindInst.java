31b571fcd832935906190929c72dbb1cd1f27c40
nwdxlgzs <47502008+nwdxlgzs@users.noreply.github.com>
2023-01-23 23:37:10 +0800

    Update bindInst.java
    增加抹除无用位置的操作数为0

--- com/nwdxlgzs/xasm/bindInst.java
+++ com/nwdxlgzs/xasm/bindInst.java
@@ -13,10 +13,40 @@ public class bindInst {
             Instruction instruction = code[i];
             OPCode op = instruction.getOpCode();
             switch (op) {
+                case OP_MOVE:
+                case OP_LOADNIL:
+                case OP_GETUPVAL:
+                case OP_SETUPVAL:
+                case OP_NEWARRAY:
+                case OP_UNM:
+                case OP_BNOT:
+                case OP_NOT:
+                case OP_LEN:
+                case OP_TAILCALL:
+                case OP_RETURN:
+                case OP_VARARG: {//填补没用的操作数C
+                    if (instruction.C() != 0) {
+                        instruction.C(0);
+                    }
+                    break;
+                }
+                case OP_TFOREACH:
+                case OP_TBC: {//填补没用的操作数B&C
+                    if (instruction.B() != 0) {
+                        instruction.B(0);
+                    }
+                    if (instruction.C() != 0) {
+                        instruction.C(0);
+                    }
+                    break;
+                }
                 case OP_LOADKX: {//OP_LOADKX特例
                     int target = i + 1;
                     if (target < sizecode) {
                         code[target].isCanJump2There = true;
+                    }
+                    if (instruction.Bx() != 0) {//填补没用的操作数Bx
+                        instruction.Bx(0);
                     }
                     break;
                 }
@@ -43,6 +73,22 @@ public class bindInst {
                     }
                     break;
                 }
+                case OP_TFORCALL: {//填补没用的操作数B以及当前指令+下一句sBx（case冲突，java怎么没goto啊）
+                    if (instruction.B() != 0) {
+                        instruction.B(0);
+                    }
+                    int target = i + 1;
+                    if (target < sizecode) {
+                        code[target].isCanJump2There = true;
+                        Instruction instruction2 = code[target];
+                        int sBx = instruction2.sBx();
+                        target = i + 2 + sBx;
+                        if (target >= 0 && target < sizecode) {
+                            code[target].isCanJump2There = true;
+                        }
+                    }
+                    break;
+                }
                 case OP_EQ:
                 case OP_LT:
                 case OP_LE: {
@@ -52,8 +98,7 @@ public class bindInst {
                     //没有break，因为还有后续处理
                 }
                 case OP_TEST:
-                case OP_TESTSET:
-                case OP_TFORCALL: {//当前指令+下一句sBx
+                case OP_TESTSET: {//当前指令+下一句sBx
                     int target = i + 1;
                     if (target < sizecode) {
                         code[target].isCanJump2There = true;
